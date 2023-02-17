586cfca3bd60939a4157465e6874eb4852bdd5a4
shojol021 <shojol021@gmail.com>
2023-02-17 17:17:13 +0600

    money-master project

--- vault.js
+++ vault.js
@@ -66,3 +66,11 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     alert('Withdraw Success!')
 
 })
+
+const product = 5; 
+const price = "7"; 
+const subTotal = product * price; 
+const tax = subTotal/10; 
+const total = subTotal + tax; 
+const category="pen"; const performer = category+"-teller";
+
